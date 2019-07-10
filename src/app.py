04f3d6147e89675a2ec537d3332f1222bb765fe8
Charlie Armentrout <carmentr@cisco.com>
2019-07-10 10:41:41 -0400

    Starting to dockerize

---
+++ src/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=os.environ.get('DEBUG', False)
+    )
